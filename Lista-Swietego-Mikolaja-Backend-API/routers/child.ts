19b78757837086a3f8fc803b3a44205d059371ee
sebuszqo <sekn.kn@gmail.com>
2023-02-11 17:26:01 +0100

    Backend project API to react - santa list of gifts

---
+++ Lista-Swietego-Mikolaja-Backend-API/routers/child.ts
@@ -0,0 +1,48 @@
+import {Router} from "express";
+import {ChildRecord} from "../records/child.record";
+import {GiftRecord} from "../records/gift.record";
+import {ValidationError} from "../utils/errors";
+
+
+export const childRouter = Router();
+
+childRouter // /child
+
+    .get('/', async (req, res) => {
+        const childrenList = await ChildRecord.listAll();
+        const giftsList = await GiftRecord.listAll();
+
+        res.json({
+            childrenList,
+            giftsList,
+        });
+    })
+
+    .post('/', async (req, res) => {
+        const newChild = new ChildRecord(req.body);
+        await newChild.insert();
+
+        res.redirect('/child');
+    })
+
+    .patch('/gift/:childId', async (req, res) => {
+        const child = await ChildRecord.getOne(req.params.childId);
+
+        if (child === null) {
+            throw new ValidationError('Nie znaleziono dziecka z podanym ID.');
+        }
+
+        const gift = req.body.giftId === '' ? null : await GiftRecord.getOne(req.body.giftId);
+
+        if (gift) {
+            if (gift.count <= await gift.countGivenGifts()) {
+                throw new ValidationError('Tego prezentu jest za mało.');
+            }
+        }
+
+        child.giftId = gift?.id ?? null;
+        await child.update();
+
+        res.redirect('/child');
+    });
+
---
+++ Lista-Swietego-Mikolaja-Backend-API/routers/gift.ts
@@ -0,0 +1,26 @@
+import {Router} from "express";
+import {GiftRecord} from "../records/gift.record";
+
+export const giftRouter = Router();
+
+giftRouter
+
+    .get('/', async (req, res) => {
+        const giftsList = await GiftRecord.listAll();
+
+        res.json({
+            giftsList,
+        });
+    })
+
+    .post('/', async (req, res) => {
+        const data = {
+            ...req.body,
+            count: Number(req.body.count),
+        };
+
+        const newGift = new GiftRecord(data);
+        await newGift.insert();
+
+        res.redirect('/gift');
+    });
