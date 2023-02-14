230df74c7ef577509ab371ab7372d063e135a4a4
sebuszqo <sekn.kn@gmail.com>
2023-02-14 15:31:54 +0100

    Added link color dealing

--- lista-swietego-mikolaja-frontend/src/App.tsx
+++ lista-swietego-mikolaja-frontend/src/App.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import './App.css';
 import {GiftsView} from "./views/GiftsView";
-import {Link, Route, Routes} from "react-router-dom";
+import {Link, NavLink, Route, Routes} from "react-router-dom";
 import {NotFoundView} from "./views/NotFoundView";
 
 function App() {
+    const colorOfLink = ({isActive}: { isActive: boolean; }) => ({color: isActive ? 'green' : 'red'})
     return (
         <>
             {/*it should be in other component too but for learning purpose it's here */}
             <h1>Santa App</h1>
-            Menu: <Link to={'/gift'}>Switch to gifts</Link> |
-            <Link to={'/'}>Switch to main</Link>
+            Menu:
+            <NavLink style={colorOfLink} to={'/gift'}>
+                Switch to gifts
+            </NavLink> |
+            <NavLink style={colorOfLink} to={'/'}>Switch to main</NavLink>
             <Routes>
                 {/*<Route path='/' element={<MainView/>}/>*/}
                 <Route path='/gift' element={<GiftsView/>}/>
