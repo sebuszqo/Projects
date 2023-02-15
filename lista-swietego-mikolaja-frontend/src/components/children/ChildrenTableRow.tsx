1dd94d3baa00995cfd338de53183293e52014c57
sebuszqo <sekn.kn@gmail.com>
2023-02-15 11:49:10 +0100

    The end of project frontend and backend - santa gifts and children list

--- lista-swietego-mikolaja-frontend/src/components/children/ChildrenTableRow.tsx
+++ lista-swietego-mikolaja-frontend/src/components/children/ChildrenTableRow.tsx
@@ -1,6 +1,7 @@
 import {ChildEntity, GiftEntity} from "types";
 import {MouseEvent} from "react";
 import {Link} from "react-router-dom";
+import {ChildGiftSelect} from "../ChildGiftSelect";
 
 interface Props {
     child: ChildEntity;
@@ -11,7 +12,10 @@ export const ChildrenTableRow = (props: Props) => {
     return (
         <tr>
             <th>{props.child.name}</th>
-            <td>{props.child.giftId}</td>
+            <td>
+                <ChildGiftSelect childId={props.child.id as string} giftsList={props.gifts}
+                                 selectedId={props.child.giftId}/>
+            </td>
         </tr>
     );
 };
