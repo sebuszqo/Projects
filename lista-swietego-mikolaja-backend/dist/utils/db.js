ee983e6778dd79b8bcb66b014d1ebb3efec0e4b8
sebuszqo <sekn.kn@gmail.com>
2023-02-15 12:02:04 +0100

    Making some file changes

--- lista-swietego-mikolaja-backend/dist/utils/db.js
+++
@@ -1,9 +0,0 @@
-import { createPool } from "mysql2/promise";
-export const pool = createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'santa-gifts',
-    namedPlaceholders: true,
-    decimalNumbers: true,
-});
-//# sourceMappingURL=db.js.map
